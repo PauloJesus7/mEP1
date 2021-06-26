7e005e5b1bc254897f70b737bc9ffd924ace5cd7
PauloJesus7 <86523673+PauloJesus7@users.noreply.github.com>
2021-06-26 10:49:15 -0300

    mEP1.c
    Arquitetura Neumann

---
+++ mEP1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+/*miniEP 1 Estrutura de Dados*/
+
+int main(){
+    int N,M;
+    int *a;
+    int *b;
+    printf("Digite o tamanho das posições na memória: ");
+    scanf("%d",&N);
+
+    printf("M=1 Soma de a b x\nM=2 Subtracao de a b x\nM=3 imprime i");
+    printf("Insira valor de M: ");
+    scanf("%d",&M);
+    
+    return 0;
+}
